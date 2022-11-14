64126f3c43adb15f8c99316004cfa2b01d2ab058
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-14 17:14:49 +0100

    Update 'vues/test.php'

--- vues/test.php
+++ vues/test.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>test page | ToDoList</title>
-  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
   <link rel="icon" type="image/x-icon" href="ressources/images/favicon.png" >
 </head>
 
