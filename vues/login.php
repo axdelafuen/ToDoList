2e262a2e9409a1bfe10ac048480fcf5e9c75568c
axdelafuen <axel.dlf@free.fr>
2022-11-16 22:16:02 +0100

    modif base codefirst

--- vues/login.php
+++ vues/login.php
@@ -10,8 +10,6 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 <body>
-    <?= getenv("MARIADB_PASSWORD")?>
-    <?= getenv("MARIADB_USER")?>
     <form method="post">
       <input class="anonymous" type="submit" value="Continue as anonymous">
       <input type="hidden" name="action" value="logAno">
