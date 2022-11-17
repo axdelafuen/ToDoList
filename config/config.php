7b1c756419cde07b9a5c33f6aeefd9fb69ca8f29
axdelafuen <axel.dlf@free.fr>
2022-11-17 19:21:48 +0100

    modif modeles/metiers

--- config/config.php
+++ config/config.php
@@ -34,9 +34,9 @@ $vues['editAccount']='vues/editAccount.php';
 
 // variables de test :
 
-require_once('modeles/ToDoList.php');
-require_once('modeles/Task.php');
-require_once('modeles/User.php');
+require_once('metiers/ToDoList.php');
+require_once('metiers/Task.php');
+require_once('metiers/User.php');
 
 $todo = array(new TodoList('MyBigToDo',array(new Task(1,1,false,'Faire cuire des pates'),new Task(2,2,true,'Manger du pain')),new User(1,'fred@fred.com','1234'),false) , new TodoList('MyBigToDo2',array(new Task(1,1,false,'Faire cuire du riz'),new Task(2,2,true,'Acheter des chaussettes')),new User(1,'fred@fred.com','1234'),true));
 
