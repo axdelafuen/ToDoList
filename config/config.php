fa6c3b49e19ec6b301b7a832e19218fee0ab462b
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-30 17:25:44 +0100

    Update 'config/config.php'

--- config/config.php
+++ config/config.php
@@ -29,9 +29,35 @@ $vues['admin']='vues/adminPanel.php';
 $vues['editAccount']='vues/editAccount.php';
 $vues['registerValide']='vues/registerValide.php';
 
+// variables de test :
 
 require_once('metiers/ToDoList.php');
 require_once('metiers/Task.php');
 require_once('metiers/User.php');
+// global var
+$user;
+$selectedToDo=0;
+// variables de test :
+$usr1=new User(1,'Catherine@php.com','4321');
+$usr2=new User(1,'fred@fred.com','1234');
+$usrAr= array(0=>$usr1,1=>$usr2);
 
+$tsk1=new Task( 1,1,false,'Faire cuire des pates');
+$tsk2 = new Task(2,2,true,'Manger du pain');
+$tsk5 = new Task(2,2,true,'Manger du pain');
+$ar1 = array();
+$ar1[]=$tsk1;
+$ar1[]=$tsk2;
+$ar1[]=$tsk5;
+
+$tsk3=new Task( 3,1,false,'Boire');
+$tsk4 = new Task(4,2,false,'Dormir');
+$ar2 = array($tsk3,$tsk4);
+
+$todo1=new ToDoList(0,"ToDo1",$ar1,$usrAr,true);
+$todo2=new ToDoList(1,"ToDo2",$ar2,$usrAr,true);
+
+$todo = array();
+$todo[]=$todo1;
+$todo[]=$todo2;
 ?>
